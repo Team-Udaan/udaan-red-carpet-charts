cb72368c8b02cb9a85d88b15f01ebd5b8560f480
maunilswadas <swadasmaunil@gmail.com>
2017-03-17 01:28:42 +0530

    Add: graphs, Status: basic UI completed

---
+++ main.js
@@ -0,0 +1,190 @@
+var risingstar = c3.generate({
+    bindto: '#rising-star',
+    data: {
+        type: 'donut',
+        columns: [
+            ['data1', 300],
+            ['data2', 530],
+            ['data3', 500],
+            ['data4', 600]
+        ]
+    },
+    donut: {
+        title: 'Rising Star',
+        width: 20,
+        label: {
+            show: false
+        }
+    }
+}).legend.hide();
+var sportsicon = c3.generate({
+    bindto: '#sports-icon',
+    data: {
+        type: 'donut',
+        columns: [
+            ['data1', 300],
+            ['data2', 530],
+            ['data3', 500],
+            ['data4', 600]
+        ]
+    },
+    donut: {
+        title: 'Sports Icon',
+        width: 20,
+        label: {
+            show: false
+        }
+    }
+}).legend.hide();
+var facem = c3.generate({
+    bindto: '#face-of-the-year-m',
+    data: {
+        type: 'donut',
+        columns: [
+            ['data1', 300],
+            ['data2', 530],
+            ['data3', 500],
+            ['data4', 600]
+        ]
+    },
+    donut: {
+        title: 'Face Of The Year-Male',
+        width: 20,
+        label: {
+            show: false
+        }
+    }
+}).legend.hide();
+var facef = c3.generate({
+    bindto: '#face-of-the-year-f',
+    data: {
+        type: 'donut',
+        columns: [
+            ['data1', 300],
+            ['data2', 530],
+            ['data3', 500],
+            ['data4', 600]
+        ]
+    },
+    donut: {
+        title: 'Face Of The Year-Female',
+        width: 20,
+        label: {
+            show: false
+        }
+    }
+}).legend.hide();
+var stylem = c3.generate({
+    bindto: '#style-icon-m',
+    data: {
+        type: 'donut',
+        columns: [
+            ['data1', 300],
+            ['data2', 530],
+            ['data3', 500],
+            ['data4', 600]
+        ]
+    },
+    donut: {
+        title: 'Style Icon-Male',
+        width: 20,
+        label: {
+            show: false
+        }
+    }
+}).legend.hide();
+var stylef = c3.generate({
+    bindto: '#style-icon-f',
+    data: {
+        type: 'donut',
+        columns: [
+            ['data1', 300],
+            ['data2', 530],
+            ['data3', 500],
+            ['data4', 600]
+        ]
+    },
+    donut: {
+        title: 'Style Icon-Female',
+        width: 20,
+        label: {
+            show: false
+        }
+    }
+}).legend.hide();
+var artistm = c3.generate({
+    bindto: '#artist-of-the-year-m',
+    data: {
+        type: 'donut',
+        columns: [
+            ['data1', 300],
+            ['data2', 530],
+            ['data3', 500],
+            ['data4', 600]
+        ]
+    },
+    donut: {
+        title: 'Artist Of The Year-Male',
+        width: 20,
+        label: {
+            show: false
+        }
+    }
+}).legend.hide();
+var artistf = c3.generate({
+    bindto: '#artist-of-the-year-f',
+    data: {
+        type: 'donut',
+        columns: [
+            ['data1', 300],
+            ['data2', 530],
+            ['data3', 500],
+            ['data4', 600]
+        ]
+    },
+    donut: {
+        title: 'Artist Of The Year-Female',
+        width: 20,
+        label: {
+            show: false
+        }
+    }
+}).legend.hide();
+var personam = c3.generate({
+    bindto: '#persona-m',
+    data: {
+        type: 'donut',
+        columns: [
+            ['data1', 300],
+            ['data2', 530],
+            ['data3', 500],
+            ['data4', 600]
+        ]
+    },
+    donut: {
+        title: 'Mr.Persona',
+        width: 20,
+        label: {
+            show: false
+        }
+    }
+}).legend.hide();
+var personaf = c3.generate({
+    bindto: '#persona-f',
+    data: {
+        type: 'donut',
+        columns: [
+            ['data1', 300],
+            ['data2', 530],
+            ['data3', 500],
+            ['data4', 600]
+        ]
+    },
+    donut: {
+        title: 'Miss.Persona',
+        width: 20,
+        label: {
+            show: false
+        }
+    }
+}).legend.hide();
